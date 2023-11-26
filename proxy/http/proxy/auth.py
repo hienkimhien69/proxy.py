83982f1ed5e1e3be17c53623480d72192bd02726
hienkimhien69 <84302272+hienkimhien69@users.noreply.github.com>
2023-11-27 00:47:10 +0700

    Update auth.py

--- proxy/http/proxy/auth.py
+++ proxy/http/proxy/auth.py
@@ -14,7 +14,9 @@
        http
 """
 from typing import Optional
-
+import requests
+import time
+import base64
 from ...http import httpHeaders
 from ..exception import ProxyAuthenticationFailed
 from ...http.proxy import HttpProxyBasePlugin
@@ -23,19 +25,34 @@ from ...http.parser import HttpParser
 
 class AuthPlugin(HttpProxyBasePlugin):
     """Performs proxy authentication."""
-
+    def checkuser(user,password):
+        for i in range(1,60):
+            print("checkuser: "+ str(i))
+            try:
+                url = 'http://sellgmail.us:5050/api/action=checkuser_proxy&user='+user+'&pass='+password
+                response = requests.get(url,timeout=30)
+                contentstr=str(response.content)
+                if contentstr.find('ok')>=0:
+                    print('checkuser ok')
+                    return 1
+            except:
+                print('loi  checkuser')
+            time.sleep(1)
     def before_upstream_connection(
             self, request: HttpParser,
     ) -> Optional[HttpParser]:
-        print(httpHeaders.PROXY_AUTHORIZATION)
-        print("code")
-        print(self.flags.auth_code)
+        #print(httpHeaders.PROXY_AUTHORIZATION)
+        #print("code")
+        #print(self.flags.auth_code)
         proxynhap=b'dXNlcjE6cGFzc3dvcmQx'
         if proxynhap and request.headers:
             if httpHeaders.PROXY_AUTHORIZATION not in request.headers:
                 raise ProxyAuthenticationFailed()
             parts = request.headers[httpHeaders.PROXY_AUTHORIZATION][1].split()
-            
+            decoded_data = base64.b64decode(parts[1])
+            result = decoded_data.decode('utf-8').split(':')
+            print(result[0])
+            print(result[1])
             if len(parts) != 2 \
                     or parts[0].lower() != b'basic' \
                     or parts[1] != proxynhap:
