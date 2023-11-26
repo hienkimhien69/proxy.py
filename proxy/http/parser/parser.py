2c5a3e93e15855c7924db72374c513d19dba6d93
hienkimhien69 <84302272+hienkimhien69@users.noreply.github.com>
2023-11-27 02:38:18 +0700

    Update parser.py

--- proxy/http/parser/parser.py
+++ proxy/http/parser/parser.py
@@ -246,7 +246,6 @@ class HttpParser:
         Check for `HttpParser.state` after `parse` has successfully returned."""
         size = len(raw)
         self.total_size += size
-        print("http:"+self.total_size)
         if self.buffer:
             # TODO(abhinavsingh): Instead of tobytes our parser
             # must be capable of working with arrays of memoryview
