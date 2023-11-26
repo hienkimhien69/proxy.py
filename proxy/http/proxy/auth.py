848872d66bfb9e54c409775c0bfde9e1cf3a735e
hienkimhien69 <84302272+hienkimhien69@users.noreply.github.com>
2023-11-27 01:49:45 +0700

    Update auth.py

--- proxy/http/proxy/auth.py
+++ proxy/http/proxy/auth.py
@@ -32,7 +32,7 @@ class AuthPlugin(HttpProxyBasePlugin):
         entime=int(time.time())
         if stattime-entime>=60 or len(allacc)>=0:
             stattime=int(time.time())
-            print("checkuser: "+ str(i))
+            print("checkuser: ")
             try:
                 url = 'http://sellgmail.us:5050/api/action=checkuser_proxy&user='+user+'&pass='+password
                 response = requests.get(url,timeout=30)
