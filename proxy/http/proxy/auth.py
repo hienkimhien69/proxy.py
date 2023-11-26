f3920f4ecf3d1ab5415a71851db49e01319a6cc1
hienkimhien69 <84302272+hienkimhien69@users.noreply.github.com>
2023-11-27 01:00:58 +0700

    Update auth.py

--- proxy/http/proxy/auth.py
+++ proxy/http/proxy/auth.py
@@ -22,39 +22,54 @@ from ..exception import ProxyAuthenticationFailed
 from ...http.proxy import HttpProxyBasePlugin
 from ...http.parser import HttpParser
 
-
+stattime=int(time.time())
+datacheck=0
 class AuthPlugin(HttpProxyBasePlugin):
     """Performs proxy authentication."""
     def checkuser(user,password):
-        for i in range(1,60):
-            print("checkuser: "+ str(i))
-            try:
-                url = 'http://sellgmail.us:5050/api/action=checkuser_proxy&user='+user+'&pass='+password
-                response = requests.get(url,timeout=30)
-                contentstr=str(response.content)
-                if contentstr.find('ok')>=0:
-                    print('checkuser ok')
-                    return 1
-            except:
-                print('loi  checkuser')
-            time.sleep(1)
+        global stattime
+        global datacheck
+        entime=int(time.time())
+        if stattime-entime>=60 or datacheck==0:
+            stattime=int(time.time())
+            for i in range(1,60):
+                print("checkuser: "+ str(i))
+                try:
+                    url = 'http://sellgmail.us:5050/api/action=checkuser_proxy&user='+user+'&pass='+password
+                    response = requests.get(url,timeout=30)
+                    contentstr=str(response.content)
+                    if contentstr.find('ok')>=0:
+                        print('checkuser ok')
+                        return 1
+                except:
+                    print('loi  checkuser')
+                time.sleep(1)
+        else:
+            return datacheck
+        
     def before_upstream_connection(
             self, request: HttpParser,
     ) -> Optional[HttpParser]:
         #print(httpHeaders.PROXY_AUTHORIZATION)
         #print("code")
         #print(self.flags.auth_code)
-        proxynhap=b'dXNlcjE6cGFzc3dvcmQx'
-        if proxynhap and request.headers:
+        global datacheck
+        if self.flags.auth_code and request.headers:
             if httpHeaders.PROXY_AUTHORIZATION not in request.headers:
                 raise ProxyAuthenticationFailed()
             parts = request.headers[httpHeaders.PROXY_AUTHORIZATION][1].split()
-            decoded_data = base64.b64decode(parts[1])
-            result = decoded_data.decode('utf-8').split(':')
-            print(result[0])
-            print(result[1])
-            if len(parts) != 2 \
-                    or parts[0].lower() != b'basic' \
-                    or parts[1] != proxynhap:
+           
+            
+            if len(parts) != 2 or parts[0].lower() != b'basic':
                 raise ProxyAuthenticationFailed()
+            else :
+                decoded_data = base64.b64decode(parts[1])
+                result = decoded_data.decode('utf-8').split(':')
+                print(result[0])
+                print(result[1])
+                datacheck=AuthPlugin.checkuser(result[0],result[1])
+                if datacheck==1:
+                    print("xac thuc ok")
+                else:
+                    raise ProxyAuthenticationFailed()
         return request
